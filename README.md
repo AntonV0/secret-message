a3728f71961b60965325349ed031ec007d8035bc
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-11-22 14:39:57 +0000

    Final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Secret Message
+## Project #28 from Codecademy's Full Stack Engineer Career Path
+### By Anton Vlasenko (AntonV0)  
+## Project Outline
+Secret Message is a JavaScript program that logs a sentence from an array of strings. This "secret" sentence is produces using various array methods.
+## What I Learned
+  - JavaScript array syntax
+  - Array methods: pop(), push(), shift(), unshift(), splice(), and join()
+***
+*Please note that in each project, the first commit is always the starter code provided by Codecademy.*
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length); //24
+secretMessage.pop();
+console.log(secretMessage.length); //23
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage.join(' '));
+
+/* Output:
+Programming is not about what you know it is about what you can figure out. -2015, Chris Pine, Learn to Program
+*/
